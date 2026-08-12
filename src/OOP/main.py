0ae051c0005d17cfa6a435a7a98f013474e2bbed
ahmedarafat-ai <ahmedarafat246810@gmail.com>
2026-08-12 16:08:34 +0300

    add OOP task solutions

---
+++ src/OOP/main.py
@@ -0,0 +1,15 @@
+
+
+def main():
+    n1 = int(input("Enter first number: "))
+    n2 = int(input("Enter second number: "))
+    c = Calc(n1, n2)
+    print("Sum =", c.sum())
+    print("Sub =", c.sub())
+    print("Mul =", c.mul())
+    if n2 == 0:
+        print("Cannot divide by zero")
+    else:
+        print("Div =", c.div())
+
+main()
